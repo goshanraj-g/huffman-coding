15ca1a3abbd20b14e3af2989e2f63759a9f4b7dd
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-03 17:43:05 -0400

    defining huffman tree node, and comparator for min heap

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 using namespace std;
+
+// defining the huffman tree node
+struct Node
+{
+    char ch;
+    int freq;
+    Node *left;
+    Node *right;
+
+    Node(char ch, int freq) : ch(ch), freq(freq), left(nullptr), right(nullptr) {}
+};
+
+// comparator for min heap
+struct Compare
+{
+    bool operator()(Node *a, Node *b)
+    {
+        return a->freq > b->freq; // smallest freq comes first ( min heap )
+    }
+};
 
 int main()
 {
@@ -15,12 +36,28 @@ int main()
 
     // output stream out.txt
     ofstream file_out("out.txt");
+    unordered_map<char, int> freq;
     string line;
 
     // getline(input_stream, string_variable)
+    // using
     while (getline(file_in, line))
     {
+        for (char c : line) // for char in the line
+        {
+            c = tolower(c);
+            if (isalpha(c))
+            {
+                freq[c]++;
+            }
+        }
         cout << line << "\n";
+    }
+
+    cout << "Hashmap" << endl;
+    for (const auto &pair : freq)
+    {
+        cout << "Key: " << pair.first << ": " << pair.second << "\n";
     }
 
     file_in.close();
